4b51a0e1bf7e04bfd6f82ef727f03fd465c063fb
Yohan Neris <Yohanneris@MacBook-Air-de-Yohan.local>
2017-10-10 19:37:49 +0200

    y new job

---
+++ 00_hello/hello.rb
@@ -0,0 +1,14 @@
+#write your code here
+
+def hello
+	"Hello!"	
+end
+
+def greet(name_temp)
+	 "Hello, #{name_temp}!"	
+end
+	
+
+
+
+
---
+++ 01_temperature/temperature.rb
@@ -0,0 +1,12 @@
+def ftoc(ftemperature)
+	((ftemperature -32)*5/9)
+end
+
+def ctof(ctemperature)
+	(((ctemperature * 9.0ls)/5)+32)
+end
+
+
+
+
+#write your code here
